1e3024d905d9237fda860946bfe26bea780dfe7a
ArthurHenriquePereira <128611300+ArthurHenriquePereira@users.noreply.github.com>
2023-06-13 19:34:48 -0300

    Add files via upload

---
+++ modelo/Funcionario.java
@@ -0,0 +1,47 @@
+package modelo;
+
+import java.util.Date;
+
+public class Funcionario {
+	private int id;
+	private String nome, cargo,telefone, cpf;
+	private double salario;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+
+}
